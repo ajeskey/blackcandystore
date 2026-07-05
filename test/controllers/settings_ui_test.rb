020acea60263f003196e08c6283b99d735e62085
Aaron Jeskey <ajeskey@gmail.com>
2026-07-05 18:20:10 -0400

    Add source-preference and DAAP/RSP settings UI
    - Source preference selector in personal settings (posts to /source_preference)
    
    - DAAP/RSP media-client toggles in admin settings; i18n + request tests

---
+++ test/controllers/settings_ui_test.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class SettingsUiTest < ActionDispatch::IntegrationTest
+  test "settings page renders for a user" do
+    login(users(:visitor1))
+
+    get setting_path
+
+    assert_response :success
+  end
+
+  test "a user can update their source preference from settings" do
+    user = users(:visitor1)
+    login(user)
+
+    patch source_preference_path,
+      params: { source_preference: "prefer_highest_quality" },
+      headers: { "HTTP_REFERER" => setting_url }
+
+    assert_response :redirect
+    assert_equal "prefer_highest_quality", user.reload.source_preference
+  end
+
+  test "an admin can toggle DAAP and RSP from settings" do
+    login(users(:admin))
+
+    patch setting_path, params: { setting: { enable_daap: "1", enable_rsp: "1" } }
+
+    assert_redirected_to setting_path
+    assert Setting.enable_daap
+    assert Setting.enable_rsp
+  end
+end
